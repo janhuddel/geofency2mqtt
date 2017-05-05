2aa829e8a36306bcf4fe7e5ec0fefdcda93c9f84
Jan Rohwer <jan.rohwer@gmail.com>
2017-05-05 15:04:43 +0200

    set parameters via cli-args

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
+// parse command-line-args
+var argv = require('minimist')(process.argv.slice(2));
+
 // mqtt-client
 var mqtt = require('mqtt');
-var mqttUrl = 'mqtt://mqtt:1883';
-var mqttClient = mqtt.connect(mqttUrl);
+var mqttServer = argv['mqtt-server'] || 'localhost';
+var mqttPort = argv['mqtt-port'] || 1883;
+var mqttUrl = 'mqtt://' + mqttServer + ':' + mqttPort;
+var mqttTopicPrefix = argv['mqtt-topic-prefix'] || 'geofency/presence/';
 
 // express-app
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var httpServerPort = 80;
-var app = express();
+var httpServerPort = argv['http-port'] || 80;
 
 // setup
+var app = express();
 app.use(bodyParser.json());
-app.use(morgan('combined'));
+if (argv['http-log']) {
+  app.use(morgan('combined'));
+}
 
+// connect to mqtt-broker
+var mqttClient = mqtt.connect(mqttUrl);
 mqttClient.on('connect', function() {
-  console.log('mqtt-client connected to ' + mqttUrl);
+  console.log('mqtt-client connected to ' + mqttUrl + '- using "'+ mqttTopicPrefix + '" as topic-prefix.');
 
   // listen after successful connection to mqtt-broker
   app.listen(httpServerPort, function () {
     console.log('geofency2mqtt is listening on port: ' + httpServerPort);
-    console.log('press ctrl-c to exit.');
   });
 
   // handle POST-request
   app.post('/geofency/:username/', function (req, res) {
     var presenceState = (req.body.entry === '1' ? 'true' : 'false');
-    mqttClient.publish('geofency/presence/' + req.params.username, presenceState);
+    mqttClient.publish(mqttTopicPrefix + req.params.username, presenceState);
     console.log('updated presence-state for user \'' + req.params.username + '\' to ' + presenceState);
     res.send('ok');
   });
